a1888cfc3587e22757ac3e82301321f1b4d83a37
Ilja M <i.meleshkevich@gmail.com>
2022-06-01 22:58:28 +0200

    add eventlisteners, point 8

--- src/js/script.js
+++ src/js/script.js
@@ -23,47 +23,60 @@ const options = ["rock", "paper", "scissors"];
 
 // func returns random result aka bot plays
 const getComputerPlay = () => {
-
   return options[Math.floor(Math.random() * options.length)];
 };
 
- console.log(getComputerPlay()); //<<< can be removed here --- same log in play() function
-
 // hardcoded input value for function, needs to be handled by buttons
-const humanPlay = "paper"
+let humanPlay = "paper";
 
-//function which will take the parameter humanPlay, calls the getComputerPlay function, write conditions based on game rules and  update the scores, choices and the result on the page.increase the scores
+//func to clean the results output before new game round
+const cleanResults = () => {
+  results_player.textContent = "You played: ";
+  results_bot.textContent = "Bot played: ";
+  results_winner.textContent = "Result of this Round: ";
+};
+
+//function which will take the parameter humanPlay, calls the getComputerPlay function,
+//write conditions based on game rules and  update the scores,
+//choices and the result on the page.increase the scores
 const play = (humanPlay) => {
+  cleanResults();
+  const computerPlay = getComputerPlay();
 
-  const computerPlay = getComputerPlay()
-  // log for value check , can be removed
-  console.log(humanPlay);
-  console.log(computerPlay);
   results_bot.textContent += computerPlay;
   results_player.textContent += humanPlay;
   if (humanPlay === computerPlay) {
-
-    results_winner.textContent += "TIE...play again"
-  } else if (humanPlay === "rock" && computerPlay === ("scissors")) {
-    results_winner.textContent += "You Won"
-    my_score++
+    results_winner.textContent += "TIE...play again";
+  } else if (humanPlay === "rock" && computerPlay === "scissors") {
+    results_winner.textContent += "You Won";
+    my_score++;
   } else if (humanPlay === "scissors" && computerPlay === "paper") {
-    results_winner.textContent += "You Won"
-    my_score++
-  }
-  else if (humanPlay === "paper" && computerPlay === "rock") {
-    results_winner.textContent += "You Won"
-    my_score++
-  }
-
-  else {
+    results_winner.textContent += "You Won";
+    my_score++;
+  } else if (humanPlay === "paper" && computerPlay === "rock") {
+    results_winner.textContent += "You Won";
+    my_score++;
+  } else {
     results_winner.textContent += "You Lost";
-    bot_score++
+    bot_score++;
   }
 
   scoreboard_bot.textContent = bot_score;
   scoreboard_you.textContent = my_score;
+};
 
-}
+//EventListeners to start new round
+btn_rock.addEventListener("click", () => {
+  humanPlay = "rock";
+  play(humanPlay);
+});
 
-play(humanPlay)
+btn_paper.addEventListener("click", () => {
+  humanPlay = "paper";
+  play(humanPlay);
+});
+
+btn_scissor.addEventListener("click", () => {
+  humanPlay = "scissor";
+  play(humanPlay);
+});
