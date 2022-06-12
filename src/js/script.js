a8d6b12a029d0962737323e2ed08780b10ea929c
Ilja M <i.meleshkevich@gmail.com>
2022-06-12 22:20:56 +0200

    added img to render with result

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,10 @@ const results_player = document.querySelector(".footer__results_player");
 const results_bot = document.querySelector(".footer__results_bot");
 const results_winner = document.querySelector(".footer__results_winner");
 
+// Footer img to show result
+const results__img_player = document.querySelector(".results__img_player");
+const results__img_bot = document.querySelector(".results__img_bot");
+
 //variables to store the score
 let my_score = 0;
 let bot_score = 0;
@@ -26,25 +30,45 @@ const getComputerPlay = () => {
   return options[Math.floor(Math.random() * options.length)];
 };
 
-// hardcoded input value for function, needs to be handled by buttons
-let humanPlay = "paper";
-
 //func to clean the results output before new game round
 const cleanResults = () => {
+  //clean human results
   results_player.textContent = "You played: ";
+  const img_human = document.querySelector(".img_human");
+  img_human ? results__img_player.removeChild(img_human) : null;
+
+  //clean bot results
   results_bot.textContent = "Bot played: ";
+  const img_bot = document.querySelector(".img_bot");
+  img_bot ? results__img_bot.removeChild(img_bot) : null;
+
   results_winner.textContent = "Result of this Round: ";
 };
 
 //function which will take the parameter humanPlay, calls the getComputerPlay function,
 //write conditions based on game rules and  update the scores,
 //choices and the result on the page.increase the scores
+
 const play = (humanPlay) => {
   cleanResults();
   const computerPlay = getComputerPlay();
 
-  results_bot.textContent += computerPlay;
-  results_player.textContent += humanPlay;
+  // Bot played - results and img
+
+  // results_bot.textContent += computerPlay;
+  const img_bot = document.createElement("img");
+  img_bot.setAttribute("src", `img/${computerPlay}_svg.svg`);
+  img_bot.setAttribute("class", "img_bot");
+  results__img_bot.appendChild(img_bot);
+
+  // Human played - results and img
+
+  // results_player.textContent += humanPlay;
+  const img_human = document.createElement("img");
+  img_human.setAttribute("src", `img/${humanPlay}_svg.svg`);
+  img_human.setAttribute("class", "img_human");
+  results__img_player.appendChild(img_human);
+
   if (humanPlay === computerPlay) {
     results_winner.textContent += "TIE...play again";
   } else if (humanPlay === "rock" && computerPlay === "scissors") {
@@ -77,6 +101,6 @@ btn_paper.addEventListener("click", () => {
 });
 
 btn_scissor.addEventListener("click", () => {
-  humanPlay = "scissor";
+  humanPlay = "scissors";
   play(humanPlay);
 });
